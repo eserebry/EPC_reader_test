f05f71ec4f5aa1dabb705792283ee3551fe60535
Elena S <elena@Admins-MacBook-Air.local>
2019-03-22 10:11:00 -0700

    commetns added

--- read.py
+++ read.py
@@ -2,59 +2,50 @@ import threading
 import queue
 import time
 
-def read_kbd_input(inputQueue):
+def read_input(inputQueue):
+    ''' Reading input from the keyboard '''
+
     print('Ready for keyboard input:')
+
     while (True):
         input_str = input()
         inputQueue.put(input_str)
 
-def main():
-    EXIT_COMMAND = "exit"
-    inputQueue = queue.Queue()
+def unique_list(input_EPC):
+    '''Creating list of the unique EPC'''
 
-    inputThread = threading.Thread(target=read_kbd_input, args=(inputQueue,), daemon=True)
-    inputThread.start()
-
-    input_EPC = [] #list of the all EPC inputed
-
-    unique_EPC = [] #list of unique EPC inputed
-
-    str = ()
-
-    start = time.time()
-
-    PERIOD_OF_TIME = 5  #program will stop in 5  seconds
-
-    while (True):
-        if (inputQueue.qsize() > 0):
-            input_str = inputQueue.get()
-            input_EPC.append(input_str)
-            #print("input_str = {}".format(input_str)) #printing string input for debug purposes
-            #print("input_EPC = {}".format(input_EPC)) #printing list of unique EPC for debug purposes
-
-        
-            if (input_str == EXIT_COMMAND):
-                print("Exiting serial terminal.")
-                break
-
-            if (time.time() > start + PERIOD_OF_TIME):
-               break
-
+    unique_EPC = [] #list of the unique EPC
 
     for x in input_EPC:
         if x not in unique_EPC:
             unique_EPC.append(x)
+
     str = '\n'.join(unique_EPC)
     print("Unique EPC:\n{}".format(str))
-    #for x in unique_EPC:
-        #print("Unique EPC: {}".format(unique_EPC))
 
-            # Insert your code here to do whatever you want with the input_str.
 
-        # The rest of your program goes here.
+def main():
+    '''Creating list of the inputed EPC during certain period of time'''
 
-    time.sleep(0.01) 
-    print("End.")
+    input_EPC = [] #list of the all EPC inputed
+    str = () #list to be converted to string
+
+    inputQueue = queue.Queue()
+    inputThread = threading.Thread(target=read_input, args=(inputQueue,), daemon=True)
+    inputThread.start()
+
+    start = time.time() #start time functiomn
+    STOP_TIME = 5  #program will stop in 5 seconds
+
+    while (True):
+        if (inputQueue.qsize() > 0):
+            input_str = inputQueue.get()
+            input_EPC.append(input_str) 
+
+            if (time.time() > start + STOP_TIME):
+               break
+
+    unique_list(input_EPC)
 
 if (__name__ == '__main__'): 
     main()
